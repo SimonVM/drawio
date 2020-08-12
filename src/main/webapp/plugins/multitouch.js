f92cb332ca6765fa18126aee537fc59b6e60cfbc
Simon Van Mierlo <simon.vanmierlo@uantwerpen.be>
2020-08-12 11:33:55 +0200

    added multitouch test

---
+++ src/main/webapp/plugins/multitouch.js
@@ -0,0 +1,13 @@
+/**
+ * Multitouch plugin.
+ */
+Draw.loadPlugin(function(ui)
+{
+	if (ui.editor.isChromelessView())
+	{
+		ui.editor.graph.click = function(me)
+		{
+			console.log("clicked!");
+		};
+	}
+}
